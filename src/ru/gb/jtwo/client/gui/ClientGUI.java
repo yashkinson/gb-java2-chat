6fbac126c5bbbf5718a19328209041709c3b8d03
yashkinson <day16@list.ru>
2018-01-23 23:44:13 +0300

    ClientGUI: собственный обработчик исключений

--- src/ru/gb/jtwo/client/gui/ClientGUI.java
+++ src/ru/gb/jtwo/client/gui/ClientGUI.java
@@ -1,10 +1,8 @@
 package ru.gb.jtwo.client.gui;
 
-
-
 import javax.swing.*;
 
-public class ClientGUI extends JFrame{
+public class ClientGUI extends JFrame implements Thread.UncaughtExceptionHandler{
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
@@ -18,7 +16,7 @@ public class ClientGUI extends JFrame{
     private static final int HEIGHT = 300;
 
     ClientGUI() {
-
+        Thread.setDefaultUncaughtExceptionHandler(this);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         setSize(WIDTH, HEIGHT);
@@ -26,5 +24,23 @@ public class ClientGUI extends JFrame{
         setTitle("Chat Client");
 
         setVisible(true);
+    }
+
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+        e.printStackTrace(); // если есть консоль то распечатать в нее сообщение
+        StackTraceElement[] stackTraceElements = e.getStackTrace(); //получаем сообщение у объекта исключения в виде массива stackTraceElements
+        String message;
+        if (stackTraceElements.length == 0) { // если массив из элементов stackTraceElements пустой, то вывести об этом сообщение
+            message = "Empty Stacktrace";
+        } else {
+            message = e.getClass().getCanonicalName() + // полное имя класса исключения
+                    ": " + e.getMessage() + "\n" +      // сообщение
+                    "\t at " + stackTraceElements[0];   // первый элемент stackTraceElements
+        }
+
+        // Показываем полученое исключение в отдельном всплывающем окошке
+        JOptionPane.showMessageDialog(this, message, "Exception", JOptionPane.ERROR_MESSAGE);
+        System.exit(1);
     }
 }
