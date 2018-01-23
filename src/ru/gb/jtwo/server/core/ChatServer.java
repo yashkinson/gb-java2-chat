6f29a812251e98c7003f3891ec5a43200688032b
yashkinson <day16@list.ru>
2018-01-23 18:24:47 +0300

    Объявили методы начала и конца работы сервера

--- src/ru/gb/jtwo/server/core/ChatServer.java
+++ src/ru/gb/jtwo/server/core/ChatServer.java
@@ -1,4 +1,11 @@
 package ru.gb.jtwo.server.core;
 
 public class ChatServer {
+    public void start(){
+        System.out.println("Сервер стартовал");
+    }
+
+    public void stop(){
+        System.out.println("Сервер остановился");
+    }
 }
