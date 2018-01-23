13fe8f6fcdfaba80e6d05189c6f5ccc33d680225
yashkinson <day16@list.ru>
2018-01-23 23:30:15 +0300

    Создание окна ClientGUI

--- src/ru/gb/jtwo/client/gui/ClientGUI.java
+++ src/ru/gb/jtwo/client/gui/ClientGUI.java
@@ -1,10 +1,10 @@
 package ru.gb.jtwo.client.gui;
 
-import ru.gb.jtwo.server.gui.ServerGUI;
+
 
 import javax.swing.*;
 
-public class ClientGUI {
+public class ClientGUI extends JFrame{
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
@@ -12,5 +12,19 @@ public class ClientGUI {
                 new ClientGUI();
             }
         });
+    }
+
+    private static final int WIDTH = 400;
+    private static final int HEIGHT = 300;
+
+    ClientGUI() {
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        setSize(WIDTH, HEIGHT);
+        setLocationRelativeTo(null);
+        setTitle("Chat Client");
+
+        setVisible(true);
     }
 }
