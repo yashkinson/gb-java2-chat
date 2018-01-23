62b701104a25c720481915c0e4aa35012f31bad1
yashkinson <day16@list.ru>
2018-01-23 19:05:44 +0300

    Установка параметров отображения окна ServerGUI

--- src/ru/gb/jtwo/server/gui/ServerGUI.java
+++ src/ru/gb/jtwo/server/gui/ServerGUI.java
@@ -1,6 +1,9 @@
 package ru.gb.jtwo.server.gui;
 
+import ru.gb.jtwo.server.core.ChatServer;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class ServerGUI extends JFrame{
     public static void main(String[] args) {
@@ -12,7 +15,24 @@ public class ServerGUI extends JFrame{
         });
     }
 
-    ServerGUI(){
+    private static final int POS_X = 1000;
+    private static final int POS_Y = 600;
+    private static final int WIDTH = 200;
+    private static final int HEIGHT = 100;
 
+    private final ChatServer chatServer = new ChatServer();
+    private final JButton btnStart = new JButton("Start");
+    private final JButton btnStop = new JButton("Stop");
+
+    ServerGUI(){
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setBounds(POS_X, POS_Y, WIDTH, HEIGHT);
+
+        setResizable(false);
+        setTitle("Chat server");
+        setAlwaysOnTop(true);
+        setLayout(new GridLayout(1, 2));
+
+        setVisible(true);
     }
 }
