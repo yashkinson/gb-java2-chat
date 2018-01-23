8fb4897f856b4b9cd1996ac8f88b59af3caa4aca
yashkinson <day16@list.ru>
2018-01-23 18:30:25 +0300

    Запускаем конструктор GUI в отдельном потоке

--- src/ru/gb/jtwo/server/gui/ServerGUI.java
+++ src/ru/gb/jtwo/server/gui/ServerGUI.java
@@ -1,4 +1,18 @@
 package ru.gb.jtwo.server.gui;
 
-public class ServerGUI {
+import javax.swing.*;
+
+public class ServerGUI extends JFrame{
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ServerGUI();
+            }
+        });
+    }
+
+    ServerGUI(){
+
+    }
 }
