d604e86176b2a006f8bbb8044e2e50df6738ae02
yashkinson <day16@list.ru>
2018-01-23 23:23:34 +0300

    Запустили ClientGUI в отдельном потоке EDT

--- src/ru/gb/jtwo/client/gui/ClientGUI.java
+++ src/ru/gb/jtwo/client/gui/ClientGUI.java
@@ -1,4 +1,16 @@
 package ru.gb.jtwo.client.gui;
 
+import ru.gb.jtwo.server.gui.ServerGUI;
+
+import javax.swing.*;
+
 public class ClientGUI {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ClientGUI();
+            }
+        });
+    }
 }
