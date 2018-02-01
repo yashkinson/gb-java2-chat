500dafed221abf9dcf23ce3a501fd7a30c3e0d1a
yashkinson <day16@list.ru>
2018-02-01 21:30:11 +0300

    SqlClient: соединение с базой данных

--- server/src/ru/gb/jtwo/chat/server/core/SqlClient.java
+++ server/src/ru/gb/jtwo/chat/server/core/SqlClient.java
@@ -1,9 +1,20 @@
 package ru.gb.jtwo.chat.server.core;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class SqlClient {
 
-    synchronized static void connect(){
+    private static Connection connection;
 
+    synchronized static void connect(){
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:chatDB.db");
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     synchronized static void disconnect(){
