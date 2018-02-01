e03f954e5ad8402e9380a5e4be3dbf0df834c0da
yashkinson <day16@list.ru>
2018-02-01 21:05:13 +0300

    SqlClien: класс для работы с базами данных

---
+++ server/src/ru/gb/jtwo/chat/server/core/SqlClient.java
@@ -0,0 +1,17 @@
+package ru.gb.jtwo.chat.server.core;
+
+public class SqlClient {
+
+    synchronized static void connect(){
+
+    }
+
+    synchronized static void disconnect(){
+
+    }
+
+    synchronized static String getNick(String login, String password) {
+        return null;
+    }
+
+}
