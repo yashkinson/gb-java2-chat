ec7a76515f3ce38090a661ad5f89900241a974a6
yashkinson <day16@list.ru>
2018-02-02 16:36:00 +0300

    SqlClient: комментирование кода

--- server/src/ru/gb/jtwo/chat/server/core/SqlClient.java
+++ server/src/ru/gb/jtwo/chat/server/core/SqlClient.java
@@ -4,14 +4,14 @@ import java.sql.*;
 
 public class SqlClient {
 
-    private static Connection connection;
-    private static Statement statement;
+    private static Connection connection;//для хранинения соединения
+    private static Statement statement;//выражение для обращение к БД
 
     synchronized static void connect(){
         try {
-            Class.forName("org.sqlite.JDBC");
-            connection = DriverManager.getConnection("jdbc:sqlite:chatDB.db");
-            statement = connection.createStatement();
+            Class.forName("org.sqlite.JDBC"); //загрузка JDBC драйвера из статической секции
+            connection = DriverManager.getConnection("jdbc:sqlite:chatDB.db");//используем драйвер jdbc для бд  sqlite и всё это будет храниться в chatDB.db
+            statement = connection.createStatement();//создаем statement для текущего соединения
         } catch (ClassNotFoundException | SQLException e) {
             throw new RuntimeException(e);
         }
@@ -28,6 +28,8 @@ public class SqlClient {
     synchronized static String getNick(String login, String password) {
         String request = "SELECT nickname FROM users WHERE login='" +
                 login + "' AND password='" + password + "'";
+        //запрос возвращает множество с результатами в виде таблицы ResultSet
+        //при возвращении указатель на текущее положение в таблице = -1
         try (ResultSet set = statement.executeQuery(request)) {
             if (set.next()) {
                 return set.getString(1);
