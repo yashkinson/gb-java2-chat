9882c7d5e503f35935df9fcfdb792ccc158c7d0d
yashkinson <day16@list.ru>
2018-02-02 17:25:27 +0300

    Messages: протокол общение клиент-сервер

---
+++ library/src/ru/iovchinnikov/gb/chat/library/Messages.java
@@ -0,0 +1,42 @@
+package ru.iovchinnikov.gb.chat.library;
+
+public class Messages {
+    /*
+/auth_request§login§password
+/auth_accept§nickname
+/auth_denied
+
+/msg_format_error
+
+/type_broadcast
+*/
+
+    public static final String DELIMITER =          "§";
+    public static final String AUTH_REQUEST =       "/auth_request";
+    public static final String AUTH_ACCEPT =        "/auth_accept";
+    public static final String AUTH_DENIED =        "/auth_denied";
+    public static final String MSG_FORMAT_ERROR =   "/msg_format_error";
+    public static final String TYPE_BROADCAST =     "/bcast";
+
+    public static String getAuthRequest(String login, String password) {
+        return AUTH_REQUEST + DELIMITER + login + DELIMITER + password;
+    }
+
+    public static String getAuthAccept(String nickname) {
+        return AUTH_ACCEPT + DELIMITER + nickname;
+    }
+
+    public static String getAuthDenied() {
+        return AUTH_DENIED;
+    }
+
+    public static String getMsgFormatError(String message) {
+        return MSG_FORMAT_ERROR + DELIMITER + message;
+    }
+
+    public static String getTypeBroadcast(String src, String message) {
+        return TYPE_BROADCAST + DELIMITER + System.currentTimeMillis() +
+                DELIMITER + src + DELIMITER + message;
+    }
+
+}
