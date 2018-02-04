e2cdcec22da17ae261a682946e8d8ae2da851164
yashkinson <day16@list.ru>
2018-02-04 22:49:05 +0300

    SocketThread: основа

--- network/src/ru/gb/jtwo/chat/network/SocketThread.java
+++ network/src/ru/gb/jtwo/chat/network/SocketThread.java
@@ -1,4 +1,29 @@
 package ru.gb.jtwo.chat.network;
 
-public class SocketThread {
+import java.net.Socket;
+
+public class SocketThread extends Thread {
+
+    private SocketThreadListener listener;
+    private  Socket socket;
+
+    SocketThread(SocketThreadListener listener, String name, Socket socket){
+        super(name);
+        this.socket = socket;
+        this.listener = listener;
+        start();
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    public void sandMessage(String message){
+
+    }
+
+    public void close(){
+
+    }
 }
